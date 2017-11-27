37ca78a6071f7e819cc2a0b8eb51885bb8ab8c15
NikoDEK-R420G <33868839+NikoDEK-R420G@users.noreply.github.com>
2017-11-27 21:44:13 +0100

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -1,10 +1,14 @@
 client_scripts {
+	'@es_extended/locale.lua',
+	'locales/fr.lua',
 	'config.lua',
 	'client/main.lua'
 }
 
-server_scripts {
+server_scripts { 
+	'@es_extended/locale.lua',
 	'@mysql-async/lib/MySQL.lua',
+	'locales/fr.lua',
 	'config.lua',
 	'server/main.lua'
 }
